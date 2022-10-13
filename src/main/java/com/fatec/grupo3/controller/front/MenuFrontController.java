74acf3132a3b2d1566fef846a1facf61a0645571
LeiteSS <estoudente.program@gmail.com>
2022-10-13 15:29:37 -0300

    Add cadastro form

--- src/main/java/com/fatec/grupo3/controller/front/MenuFrontController.java
+++ src/main/java/com/fatec/grupo3/controller/front/MenuFrontController.java
@@ -1,9 +1,12 @@
 package com.fatec.grupo3.controller.front;
 
+import com.fatec.grupo3.model.entities.Cliente;
 import com.fatec.grupo3.model.entities.Login;
 import com.fatec.grupo3.model.entities.Usuario;
 import com.fatec.grupo3.model.service.UsuariosService;
 import com.fatec.grupo3.security.Profiles;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
@@ -14,11 +17,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MenuFrontController {
     @Autowired
     private UsuariosService service;
+
+    Logger logger = LogManager.getLogger(ClientesFrontController.class);
 
     @GetMapping("/")
     public ModelAndView login() {
@@ -40,6 +49,35 @@ public class MenuFrontController {
         res.addCookie(cookie);
 
         return "redirect:/home";
+    }
+
+
+    @GetMapping("/signUp")
+    public ModelAndView signUp(Usuario usuario) {
+        ModelAndView mv = new ModelAndView("cadastrarUsuario");
+        mv.addObject("usuario", usuario);
+        return mv;
+    }
+
+    @PostMapping("/signUp")
+    public ModelAndView save(@Valid Usuario usuario, BindingResult result) {
+        ModelAndView mv = new ModelAndView("consultarUsuario");
+        List<String> roles = new ArrayList<>();
+        roles.add("ADMIN");
+        usuario.setRoles(roles);
+        if (result.hasErrors()) {
+            mv.setViewName("cadastrarUsuario");
+        } else {
+            Usuario usuarioSaved = service.cadastrar(usuario);
+            if (usuarioSaved != null) {
+                mv.setViewName("paginaLogin");
+            } else {
+                logger.info(">>>>>> controller cadastrar com dados invalidos");
+                mv.setViewName("cadastrarUsuario");
+                mv.addObject("message", "Dados invalidos");
+            }
+        }
+        return mv;
     }
 
     @GetMapping("/home")
