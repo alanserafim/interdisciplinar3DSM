ebb103771ab6188dd2d97979842bb7d812ff70e0
LeiteSS <estoudente.program@gmail.com>
2022-11-09 21:14:21 +0900

    Application crash?

--- src/main/java/com/fatec/grupo3/Grupo3Application.java
+++ src/main/java/com/fatec/grupo3/Grupo3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Grupo3Application {
-
+// Tem que saber porque a aplication crash as vezes
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo3Application.class, args);
 	}
